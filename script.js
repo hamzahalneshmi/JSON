ff280e09af15c9c5d5fb05d0882b880d1759d57b
Hamzah Murad Mohammed Al-neshmi <hamzah.murad.mohammed.al-neshmi@ericsson.com>
2024-08-28 11:18:57 +0200

    new

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function fetchData() {
             // Replace placeholders in the entire HTML with the corresponding data
             $('body').html(function(index, html) {
                 return html
-                    .replace(/RepoName/g, data.repository_full_name)
-                    .replace(/Details1/g, '<button type=button id=detailsButton class=btn btn-primary data-bs-toggle=modal data-bs-target=#detailsModal>Details</button>')
-                    .replace(/LastUpdated/g, new Date(data.started_at).toLocaleString())
+                    .replace(/RepoName1/g, data.repository_full_name)
+                    .replace(/Details1/g, '<button type=button id=detailsButton class=btn data-bs-toggle=modal data-bs-target=#detailsModal>Details</button>')
+                    .replace(/LastUpdated1/g, new Date(data.started_at).toLocaleString())
                     .replace(/Status1/g, data.status);
             });
             // Update modal with JSON data when button is clicked
@@ -17,10 +17,15 @@ function fetchData() {
                 $('#workflowJobId').text(data.workflow_job_id);
                 $('#workflowName').text(data.workflow_name);
                 $('#headBranch').text(data.head_branch);
+                $('#senderlogin').text(data.sender_login);
                 $('#status').text(data.status);
                 $('#repositoryFullName').text(data.repository_full_name);
                 $('#startedAt').text(new Date(data.started_at).toLocaleString());
                 $('#htmlUrl').attr('href', data.html_url);
+                $('#version1').text(data.version1);
+                $('#version2').text(data.version2);
+                $('#version3').text(data.version3);
+                $('#version4').text(data.version4);
             });
         },
         error: function() {
