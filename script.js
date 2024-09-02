64bb79daf481b060e4645a55d1121e4363cd3c5e
Hamzah Murad Mohammed Al-neshmi <hamzah.murad.mohammed.al-neshmi@ericsson.com>
2024-09-02 10:03:39 +0200

    new json

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ function fetchData() {
                     .replace(/RepoName1/g, data.repository_full_name)
                     .replace(/Details1/g, '<button type=button id=detailsButton class=btn data-bs-toggle=modal data-bs-target=#detailsModal>Details</button>')
                     .replace(/LastUpdated1/g, new Date(data.started_at).toLocaleString())
-                    .replace(/Status1/g, data.status);
+                    .replace(/Status1/g, data.status)
+                    .replace(/version1/g, data.version1)
+                    .replace(/version2/g, data.version2)
+                    .replace(/version3/g, data.version3)
+                    .replace(/version4/g, data.version4);
             });
             // Update modal with JSON data when button is clicked
             $('#detailsButton').on('click', function() {
@@ -22,10 +26,6 @@ function fetchData() {
                 $('#repositoryFullName').text(data.repository_full_name);
                 $('#startedAt').text(new Date(data.started_at).toLocaleString());
                 $('#htmlUrl').attr('href', data.html_url);
-                $('#version1').text(data.version1);
-                $('#version2').text(data.version2);
-                $('#version3').text(data.version3);
-                $('#version4').text(data.version4);
             });
         },
         error: function() {
@@ -36,5 +36,5 @@ function fetchData() {
 
 $(document).ready(function() {
     fetchData(); // Fetch and display the data immediately
-    setInterval(fetchData, 10000); // Optionally, refresh the data every 10 seconds
+    //setInterval(fetchData, 10000); // Optionally, refresh the data every 10 seconds
 });
